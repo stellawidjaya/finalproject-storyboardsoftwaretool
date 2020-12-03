18261d783508a1e2123d4ef181d2f8ce61e69d48
stellawidjaya <71691496+stellawidjaya@users.noreply.github.com>
2020-12-02 22:12:55 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
-let numBoards = 12;
+
+let numBoards = 20;
 let artboards = [];
 let thumbnails = [];
+let temp = [];
+let displayKey;
+
+let init = true;
 
 let scrollbar;
 
+let brushSlider;
 let inputTitle;
 let inputCaption;
 let addButton;
@@ -14,13 +20,50 @@ let triangleButton;
 
 let s;
 let transparency = 0;
+let ind_red = 0;
 
-let strokes = [];
-let drawStrokes = false;
+let drawing = [];
+let currentPath = [];
+let isDrawing;
+
+let save;
+var database;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    //background(50);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.mousePressed(startPath);
+    canvas.parent('canvascontainer');
+    canvas.mouseReleased(endPath);
+    
+    var saveButton = select('#saveButton');
+    saveButton.mousePressed(saveDrawing);
+  
+    //start firebase
+    var firebaseConfig = {
+    apiKey: "AIzaSyCFRYCFf94YIclONyOKRzeUBFwbh7am4LE",
+    authDomain: "my-storyboard-software.firebaseapp.com",
+    databaseURL: "https://my-storyboard-software.firebaseio.com",
+    projectId: "my-storyboard-software",
+    storageBucket: "my-storyboard-software.appspot.com",
+    messagingSenderId: "625029995907",
+    appId: "1:625029995907:web:e99107e955033c685f2ab6",
+    measurementId: "G-026KHL8HLB"
+    };
+    
+    // initialize firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    database = firebase.database();
+    console.log(database);
+    var ref = database.ref('drawings');
+    ref.on('value', gotData, errorData);
+    console.log(firebase);
+  
+    //let drawings = database.val();
+    /*console.log(drawings);
+    let keys = Object.keys(drawings);
+    for (let i=0; i < keys.length; i++) {
+        var key = keys[i];*/
     
     //create artboards+thumbnails
     for (let i=0; i<numBoards; i++) {
@@ -41,7 +84,12 @@ function setup() {
     caption.style('fontFamily', 'Roboto Mono');
     caption.style('fontSize', '10px');
     
-    inputCaption = createInput();
+    inputCaption = createInput();    
+    
+    //create dom for brushes
+    brushSlider = createSlider(0,10,5,1);
+        change_brushstroke = brushSlider.value();
+
     
     //create buttons for shapes
     squareButton = createButton('SQUARE');
@@ -55,25 +103,38 @@ function setup() {
     squareButton.class('shapebuttons');
     circleButton.class('shapebuttons');
     triangleButton.class('shapebuttons');
-  
+      
     repositionAll();
+    
+    displayKey = temp[0];
+
     
     //create scrollbar
     scrollbar = new Scrollbar(width-20, 0, 10, height);
   
-  s = new Scribble();
+    s = new Scribble();
 }
 
 function shapeSquare() {
-  transparency = 255;
+    transparency = 255;
 }
 
 function shapeCircle() {
-  transparency = 0;
+    transparency = 0;
 }
 
 function shapeTriangle() {
-  
+    
+}
+
+function startPath() {
+    isDrawing = true;
+    currentPath = [];
+    drawing.push(currentPath);
+}
+
+function endPath() {
+    isDrawing = false;
 }
 
 function draw() {  
@@ -97,7 +158,8 @@ function draw() {
         
         thumbnails[i].change_y(25+(75*i));
         thumbnails[i].display();
-        thumbnails[i].mouseControl();
+        thumbnails[i].mouseControl(i);
+        thumbnails[i].set_html(temp[i]);
         
         fill(255);
         textFont('Roboto Mono');
@@ -105,6 +167,25 @@ function draw() {
         textAlign(CENTER, CENTER);
         text(i+1, width-165, 55+(75*i));  
     }
+    
+    //displayKey = thumbnails[0].get_html();
+    
+    //save drawing every second
+    let time = 0;
+    
+    if (millis() >= 2000+time) {
+        saveDrawing(displayKey);
+        time = millis();
+        console.log(displayKey);
+    }
+    
+    /*for(let x=0; x<=2; x++) {
+        if (time % 2000 == 0) {
+            x++;
+            console.log(x);
+            saveDrawing(data);
+        }
+    }*/
     
     //draw scrollbar
     scrollbar.update();
@@ -118,24 +199,36 @@ function draw() {
     s.scribbleRect(300, 300, 50, 50);
     pop();
     
-    //draw brushstroke
-    push();
-    if(drawStrokes) {
-        strokes.push(createVector(mouseX, mouseY));
+    //draw brushstroke  
+    var value = brushSlider.value();
+    
+    if (mouseIsPressed) {
+        var point = {
+            x: mouseX,
+            y: mouseY,
+        }
+        
+        if (mouseX >= 100 && mouseX <= 682 && mouseY >= height/2-180 && mouseY <= height/2+180) {
+            currentPath.push(point);
+        }
     }
     
-    strokeWeight(5);
-    stroke(50);
-    noFill();
-    beginShape();
-    for (let i=0; i<strokes.length; i++) {
-        let x = strokes[i].x;
-        let y = strokes[i].y;
+    push();
+    
+    for (let i=0; i<drawing.length; i++) {
+        var path = drawing[i];
         
-        vertex(x, y);
+        strokeWeight(value);
+        stroke(50);
+        noFill();
+        beginShape();
+        for (let j=0; j<path.length; j++) {
+            vertex(path[j].x, path[j].y);
+        }
+        endShape();    
     }
-    endShape();
     pop();
+    
 }
 
 class Board {
@@ -159,26 +252,47 @@ class Thumbnail {
         this.y = 25;
         this.width = 97;
         this.height = 60;
+        this.transparency = 0;
+        this.html;
     }
     
     change_y(y){
         this.y = y;
     }
+    
+    change_trans(trans){
+        this.transparency = trans;
+    }
         
     display() {
-        noStroke();
+        strokeWeight(2.5);
+        stroke(255,0,0, this.transparency);
         fill(255);
         //rectMode(CENTER);
         rect(this.x, this.y, this.width, this.height, 2.5, 2.5, 2.5, 2.5);
     }
     
-    mouseControl() {
-        if (mouseX >= this.x && mouseX <= this.x+this.width && mouseY >= this.y && mouseY <= this.y+this.height) {
-            console.log('1');
+    mouseControl(i) {
+        if (mouseX >= this.x && mouseX <= this.x+this.width && mouseY >= this.y && mouseY <= this.y+this.height && mouseIsPressed) {
+            thumbnails[ind_red].change_trans(0);
+            ind_red = i;
+            showDrawing(this.html);
+            //numBoards++;
+            this.transparency = 255;
+            //console.log('1');
+            displayKey = this.html;
         }
     }
-}
     
+    set_html(html){
+        this.html = html;
+    }
+    
+    get_html() {
+        return this.html;
+    }
+}
+
 class Scrollbar {
     constructor(barX, barY, barW, barH) {
         this.barX = barX;
@@ -254,13 +368,71 @@ function drawTools() {
     triangle(525, 10, 540, 40, 510, 40);
 }
 
-function mousePressed() {
-    drawStrokes = true;
-    strokes = [];
+function saveDrawing(key) {
+    let ref = database.ref('drawings');
+    let data = {
+        name: "stella",
+        drawing: drawing,
+    }
+    
+    ref.child(key).update({'drawing': drawing})
+    
+    //var updates = {};
+    //updates['/drawing/' + key] = data;
+    //ref.update(updates);
+    //ref.push(data);
 }
 
-function mouseReleased() {
-    drawStrokes = false;
+function gotData(data) {
+    let elts = selectAll('.listing');
+    for (let i=0; i < elts.length; i++) {
+        elts[i].remove();
+    }
+    
+    let drawings = data.val();
+    //console.log(drawings);
+    let keys = Object.keys(drawings);
+    for (let i=0; i < keys.length; i++) {
+        var key = keys[i];
+        append(temp, key);
+        //var li = createElement('li', '');
+        //li.class('listing');
+        //var ahref = createA('#', key);
+        //ahref.mousePressed(showDrawing);
+        //ahref.parent(li);
+        //li.parent('drawinglist');
+    }
+    
+    if(init) {
+        displayKey = temp[0];
+        showDrawing(temp[0]);
+        init = false;
+    }
+}
+
+function errorData(error) {
+    console.log(error);
+}
+
+function showDrawing(key) {
+    //var key = this.html();
+    //print(key);
+    var ref = database.ref('drawings/'+key); 
+    ref.once('value', oneDrawing, errorData);
+
+    function oneDrawing(data) {
+        var drawings = data.val();
+        drawing = drawings.drawing;
+        //console.log(drawing);
+    }
+}
+
+function mouseDragged() {
+    let value = brushSlider.value();
+    
+    strokeWeight(value);
+    stroke(50,50,50);
+    line(pmouseX, pmouseY, mouseX, mouseY);
 }
 
 function repositionAll() {
@@ -271,7 +443,10 @@ function repositionAll() {
     caption.position(100,550);
     inputCaption.position(100, 575);
     inputCaption.size(576,25);
-  
+    
+    brushSlider.position(200,25);
+    brushSlider.size(100,25);
+    
     squareButton.position(400, 0);
     circleButton.position(450, 0);
     triangleButton.position(500, 0);
@@ -283,5 +458,4 @@ function repositionAll() {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     repositionAll();
-
 }
